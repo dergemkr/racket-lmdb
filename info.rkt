81161cd1dbf70e0cf3dabf7934290baa2afc3b1f
Daniel Rothfus <self@danielrothfus.com>
2023-08-28 22:16:41 -0500

    Add info.rkt

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+
+(define collection "lmdb")
+(define deps  '(["base" #:version "8.4.0.3"]))
+(define build-deps '("rackunit-lib"))
+(define pkg-desc "A wrapper for the LMDB database.")
+(define version "0.1")
+(define pkg-authors '(danielrothfus))
+(define license 'MIT)
